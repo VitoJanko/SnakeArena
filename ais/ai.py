855cc053af06c0499eb66062249153bb43e5aef2
Sebastjan Skrbinšek <58915286+Sneezles@users.noreply.github.com>
2024-06-06 12:09:44 +0200

    Osnoven commit

--- ais/ai.py
+++ ais/ai.py
@@ -1,7 +1,8 @@
 import random
-from game_objects.snake import Snake
-from constants import DIRECTIONS
 
+from constants import DIRECTIONS
+from game_objects.snake import Snake
+from copy import deepcopy
 
 class BaseAI:
 
@@ -43,11 +44,11 @@ class AlternativeAI:
                 break
 
         return direction
-
-
+    
+    
+NUM_MOVES = 50
 class CuddleAI:
-    def get_direction(self, grid, number, positions, current_direction, possible_directions, snakes: list[Snake]):
-        position = positions[number]
+    def get_direction_internal(self, grid, number, position, current_direction, possible_directions):
         possibilities = []
         for direction in possible_directions:
             dir_x, dir_y = DIRECTIONS[direction]
@@ -58,6 +59,46 @@ class CuddleAI:
                 possibilities.append((direction, number_of_neighbors(new_head[0], new_head[1], grid, number)))
         possibilities.sort(key=lambda x: x[1], reverse=True)
         return possibilities[0][0]
+    
+    def dummy(self, grid, number, positions, possible_directions):
+        print(positions)
+        position = positions[number] 
+        for i in range(NUM_MOVES):
+            position_str = self.get_direction_internal(grid, number, position, None, possible_directions) 
+            xd, yd = DIRECTIONS[position_str]
+            new_head = (position[0] + xd, position[1] + yd)
+            
+            if collision(new_head[0], new_head[1], grid):
+                return i
+            grid[new_head[0]][new_head[1]] = number
+            position = new_head
+        return NUM_MOVES
+
+    def get_direction(self, grid, number, positions, current_direction, possible_directions, snakes):
+   
+        moves_dict = []
+        
+        for possible_direction in possible_directions:
+            backup_grid = deepcopy(grid)
+            num_moves = self.dummy(backup_grid, number, positions, DIRECTIONS.keys())
+            
+            moves_dict.append((possible_direction, num_moves))
+            
+        moves_dict.sort(key=lambda x: x[1], reverse=True)
+        
+        best_move=moves_dict[0]
+ 
+        default_move =  self.get_direction_internal(grid, number, positions[number], current_direction, possible_directions)         
+        
+        sum = 0
+        for move in moves_dict:
+            sum += move[1]
+            
+        if sum == 3*NUM_MOVES:
+            return default_move
+        else:
+            return best_move[0]
+
 
 
 def collision(head_x, head_y, grid):
